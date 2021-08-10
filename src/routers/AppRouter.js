90ef27af529c5d7aefbc2381875d64407d7e5dd7
Angel Agelvis <agelvisangel@gmail.com>
2021-08-10 09:08:01 -0500

    App Adopcion Sin Crud

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -10,20 +10,53 @@ import {
 import AdopcionHome from '../container/AdopcionHome'
 import AddPets from '../components/AddPets'
 import Navbar from '../components/Navbar'
-import Login from '../components/Login'
+import PublicRoute from './PublicRoute'
+import AuthRouter from './AuthRouter'
+import PrivateRoute from './PrivateRoute'
+
 
 const AppRouter = () => {
+    const [checking, setCheking] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(true)
+
+    useEffect(() => {
+        setCheking(false)
+
+    }, [])
+    
+    if (cheking) {
+        return (
+            <h1>Cargando ...</h1>
+        )
+    }
+
     return (
         <ChakraProvider theme={theme}>
             <Router>
                 <Navbar />
                 <Switch>
-                    <Route exact path="/login"  component={Login}/>
-                    <Route exact path="/pets" component={AdopcionHome}/>
-                    <Route exact path="/new" component={AddPets}/>
-                    <Route exact path="/" />
+                    <PublicRoute 
+                        path="/login"
+                        component={AuthRouter}
+                        isAuthenticated={isLoggedIn}
+                    />
+
+                    <PrivateRoute 
+                        exact 
+                        path="/" 
+                        component={AdopcionHome}
+                        isAuthenticated={isLoggedIn}
+                    />
+
+                    <PrivateRoute 
+                        exact 
+                        path="/new" 
+                        component={AddPets}
+                        isAuthenticated={isLoggedIn}
+                        />
+                    <Redirect to="/auth/login" />
                 </Switch>
-                <Redirect to="/" />
+                
             </Router>
         </ChakraProvider>
     )
